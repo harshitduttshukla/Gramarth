665f4f33bb9079ee9badf44a6f6a52763050fe3f
Harshit Shukla <harshitshukla6388@gmail.com>
2024-10-17 13:22:21 +0530

    heaer is add

---
+++ Gramarth-Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='shadow static z-50 top-0'>
+        <nav className='bg-gray-800 border-gray-200 px-4 lg:px-4 py-2.5'>
+            <div className='flex flex-wrap justify-between item-center mx-auto max-w-screen-xl'>
+                <Link to='/' className="flex items-center">
+                
+                
+                </Link>
+            </div>
+        </nav>    
+    </header>
+  )
+}
+
+export default Header
